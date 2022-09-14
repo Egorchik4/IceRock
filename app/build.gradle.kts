0881148d6df7f361c015fe04b81449211b277562
edd-2 <EgorDanyluk72@mail.ru>
2022-09-14 10:59:52 +0700

    Fix small bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.example.icerock"
         minSdkVersion(21)
-        targetSdkVersion(21)
+        targetSdkVersion(33)
         versionCode = 1
         versionName = "1.0"
 
